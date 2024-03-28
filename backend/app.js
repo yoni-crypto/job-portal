c9470aa86ecaa04d268f1dfabaf62b5024f9da62
yoni-crypto <yonidisu111@gmail.com>
2024-03-28 09:54:42 +0300

    last commit

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000","https://etjobs.onrender.com"] 
+}));
 
 
 //ROUTES MIDDLEWARE
